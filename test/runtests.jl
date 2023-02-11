72b76905d9a75e1657a8f40e3bc0a9f91a5a1409
Himanshu <waitasecant@gmail.com>
2023-02-11 13:53:21 +0530

    Some tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,20 @@
 using Bess
 using Test
+using CSV, DataFrames
+brca = CSV.read("D:\\Julia\\Sem3\\Seminar Report Code\\Logistic Regression\\new_brca.csv", DataFrame, normalizenames=true)
 
-@testset "Best_Subset" begin
-
-    brca = CSV.read("D:\\Julia\\Sem3\\Seminar Report Code\\Logistic Regression\\new_brca.csv", DataFrame, normalizenames=true)
+@testset "Basic Test" begin
     @test size(brca)[1] == 510
+end
+
+@testset "Best Subset Selection" begin
+    @test Bess.best_subset_selection(brca) isa Array
+end
+
+@testset "Forward Step-wise Selection" begin
+    @test Bess.forward_stepwise_selection(brca) isa Array
+end
+
+@testset "Backward Step-wise Selection" begin
+    @test Bess.backward_stepwise_selection(brca) isa Array
 end
