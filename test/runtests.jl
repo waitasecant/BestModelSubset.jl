76fd2f986548962dd1ae903260ea0a4fbdc34b86
Himanshu <waitasecant@gmail.com>
2023-02-18 14:33:08 +0530

    Minor test change!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,6 @@
 using Bess
 using Test
 using CSV, DataFrames
-brca = CSV.read("D:\\Julia\\Sem3\\Seminar Report Code\\Logistic Regression\\new_brca.csv", DataFrame, normalizenames=true)
-
-@testset "Basic Test" begin
-    @test size(brca)[1] == 510
-end
 
 @testset "Best Subset Selection" begin
     @test Bess.best_subset_selection(brca) isa Array
