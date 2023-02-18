fb256d61c2ea2bec16fb24c6c18f1f72ace99835
Himanshu <waitasecant@gmail.com>
2023-02-18 15:01:10 +0530

    Some minor changes!

--- src/Bess.jl
+++ src/Bess.jl
@@ -8,4 +8,13 @@ using MLBase
 include("best_subset.jl")
 include("forward_stepwise.jl")
 include("backward_stepwise.jl")
+
+# struct ModelSelection
+#     algorithm::Function
+#     param1::Function
+#     param2::Function
+# end
+# typeof(Bess.best_subset_selection)
+# model1 = ModelSelection(Bess.best_subset_selection, MLBase.r2, MLBase.aic)
+# println(model1)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,17 @@
 using Bess
 using Test
-using CSV, DataFrames
+using CSV, DataFrames, UrlDownload
+
+df = urldownload("https://raw.githubusercontent.com/waitasecant/Bess.jl/main/test/new_brca.csv")
 
 @testset "Best Subset Selection" begin
-    @test Bess.best_subset_selection(brca) isa Array
+    @test Bess.best_subset_selection(df) isa Array
 end
 
 @testset "Forward Step-wise Selection" begin
-    @test Bess.forward_stepwise_selection(brca) isa Array
+    @test Bess.forward_stepwise_selection(df) isa Array
 end
 
 @testset "Backward Step-wise Selection" begin
-    @test Bess.backward_stepwise_selection(brca) isa Array
+    @test Bess.backward_stepwise_selection(df) isa Array
 end
