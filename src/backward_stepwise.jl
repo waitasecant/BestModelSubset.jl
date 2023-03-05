66b9b010d240c5087f3116dbdd849463f11056ad
Himanshu <waitasecant@gmail.com>
2023-03-05 16:57:32 +0530

    Some dispatches to handles arrays

--- src/backward_stepwise.jl
+++ src/backward_stepwise.jl
@@ -12,3 +12,19 @@ function backward_stepwise_selection(df::DataFrame)
     end
     return dev
 end
+
+function backward_stepwise_selection(df::Matrix{Float64})
+    df = DataFrame(df, :auto)
+    dev = [[s for s in 1:length(names(df))-1]]
+    comb = collect(combinations(dev[1], length(names(df)) - 2))
+    while length(dev[end]) != 1
+        val = []
+        for j in comb
+            logreg = glm(Array(df[:, j]), Array(df[:, 15]), Binomial(), ProbitLink())
+            push!(val, deviance(logreg))
+        end
+        push!(dev, comb[indexin(minimum(val), val)][1])
+        comb = collect(combinations(dev[end], length(dev[end]) - 1))
+    end
+    return dev
+end
--- src/best_subset.jl
+++ src/best_subset.jl
@@ -10,3 +10,17 @@ function best_subset_selection(df::DataFrame)
     end
     return [dev[i][1] for i in 1:length(names(df))-1]
 end
+
+function best_subset_selection(df::Matrix{Float64})
+    dev = []
+    df = DataFrame(df, :auto)
+    for num in 1:length(names(df))-1
+        val = []
+        for i in collect(combinations(1:length(names(df))-1, num))
+            logreg = glm(Array(df[:, i]), Array(df[:, 15]), Binomial(), ProbitLink())
+            push!(val, deviance(logreg))
+        end
+        push!(dev, collect(combinations(1:length(names(df))-1, num))[indexin(minimum(val), val)])
+    end
+    return [dev[i][1] for i in 1:length(names(df))-1]
+end
