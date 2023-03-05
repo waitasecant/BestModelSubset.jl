89cb11e7bbe609f89edc2e0dceba570dfa1c6b35
Himanshu <waitasecant@gmail.com>
2023-03-05 17:07:40 +0530

    Testing the array dispatch!

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,18 @@ using Bess
 using Test
 using CSV, DataFrames, UrlDownload
 
-df = DataFrame(urldownload("https://raw.githubusercontent.com/waitasecant/Bess.jl/main/test/new_brca.csv"))
-
+df = CSV.read("C:\\Users\\Dell\\.julia\\dev\\Bess\\test\\new_brca.csv", DataFrame, normalizenames=true);
 @testset "Best Subset Selection" begin
     @test Bess.best_subset_selection(df) isa Array
+    @test Bess.best_subset_selection(Array(df)) isa Array
 end
 
 @testset "Forward Step-wise Selection" begin
     @test Bess.forward_stepwise_selection(df) isa Array
+    @test Bess.forward_stepwise_selection(Array(df)) isa Array
 end
 
 @testset "Backward Step-wise Selection" begin
     @test Bess.backward_stepwise_selection(df) isa Array
+    @test Bess.backward_stepwise_selection(Array(df)) isa Array
 end
