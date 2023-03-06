c0037aad0933b423cd1cf6bab6099b4237eb3fb2
Himanshu <waitasecant@gmail.com>
2023-03-06 12:18:55 +0530

    Testing the size of arrays before execution!

--- src/backward_stepwise.jl
+++ src/backward_stepwise.jl
@@ -1,30 +1,38 @@
 function backward_stepwise_selection(df::DataFrame)
-    dev = [[s for s in 1:length(names(df))-1]]
-    comb = collect(combinations(dev[1], length(names(df)) - 2))
-    while length(dev[end]) != 1
-        val = []
-        for j in comb
-            logreg = glm(Array(df[:, j]), Array(df[:, names(df)[end]]), Binomial(), ProbitLink())
-            push!(val, deviance(logreg))
+    if size(df)[1] > size(df)[2]
+        dev = [[s for s in 1:length(names(df))-1]]
+        comb = collect(combinations(dev[1], length(names(df)) - 2))
+        while length(dev[end]) != 1
+            val = []
+            for j in comb
+                logreg = glm(Array(df[:, j]), Array(df[:, names(df)[end]]), Binomial(), ProbitLink())
+                push!(val, deviance(logreg))
+            end
+            push!(dev, comb[indexin(minimum(val), val)][1])
+            comb = collect(combinations(dev[end], length(dev[end]) - 1))
         end
-        push!(dev, comb[indexin(minimum(val), val)][1])
-        comb = collect(combinations(dev[end], length(dev[end]) - 1))
+        return dev
+    else
+        forward_stepwise_selection(df::DataFrame)
     end
-    return dev
 end
 
 function backward_stepwise_selection(df::Matrix{Float64})
-    df = DataFrame(df, :auto)
-    dev = [[s for s in 1:length(names(df))-1]]
-    comb = collect(combinations(dev[1], length(names(df)) - 2))
-    while length(dev[end]) != 1
-        val = []
-        for j in comb
-            logreg = glm(Array(df[:, j]), Array(df[:, names(df)[end]]), Binomial(), ProbitLink())
-            push!(val, deviance(logreg))
+    if size(df)[1] > size(df)[2]
+        df = DataFrame(df, :auto)
+        dev = [[s for s in 1:length(names(df))-1]]
+        comb = collect(combinations(dev[1], length(names(df)) - 2))
+        while length(dev[end]) != 1
+            val = []
+            for j in comb
+                logreg = glm(Array(df[:, j]), Array(df[:, names(df)[end]]), Binomial(), ProbitLink())
+                push!(val, deviance(logreg))
+            end
+            push!(dev, comb[indexin(minimum(val), val)][1])
+            comb = collect(combinations(dev[end], length(dev[end]) - 1))
         end
-        push!(dev, comb[indexin(minimum(val), val)][1])
-        comb = collect(combinations(dev[end], length(dev[end]) - 1))
+        return dev
+    else
+        forward_stepwise_selection(df::DataFrame)
     end
-    return dev
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,21 @@ using Bess
 using Test
 using CSV, DataFrames, UrlDownload
 
-df = DataFrame(urldownload("https://raw.githubusercontent.com/waitasecant/Bess.jl/main/test/new_brca.csv"))
-
+df1 = DataFrame(urldownload("https://raw.githubusercontent.com/waitasecant/Bess.jl/main/test/new_brca.csv"))
+df2 = hcat(rand(Float64, (12, 20)), rand([0, 1], (12, 1)))
 @testset "Best Subset Selection" begin
-    @test Bess.best_subset_selection(df) isa Array
-    @test Bess.best_subset_selection(Array(df)) isa Array
+    @test Bess.best_subset_selection(df1) isa Array
+    @test Bess.best_subset_selection(Array(df1)) isa Array
+    @test Bess.best_subset_selection(df2) isa Array
 end
 
 @testset "Forward Step-wise Selection" begin
-    @test Bess.forward_stepwise_selection(df) isa Array
-    @test Bess.forward_stepwise_selection(Array(df)) isa Array
+    @test Bess.forward_stepwise_selection(df1) isa Array
+    @test Bess.forward_stepwise_selection(Array(df1)) isa Array
 end
 
 @testset "Backward Step-wise Selection" begin
-    @test Bess.backward_stepwise_selection(df) isa Array
-    @test Bess.backward_stepwise_selection(Array(df)) isa Array
+    @test Bess.backward_stepwise_selection(df1) isa Array
+    @test Bess.backward_stepwise_selection(Array(df1)) isa Array
+    @test Bess.backward_subset_selection(df2) isa Array
 end
