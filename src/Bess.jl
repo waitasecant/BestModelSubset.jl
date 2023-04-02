05d534ca1ca18a98f38a4f553d41362188b3133c
Himanshu <waitasecant@gmail.com>
2023-04-02 10:58:19 +0530

    Doll up the code

--- src/Bess.jl
+++ src/Bess.jl
@@ -14,7 +14,8 @@ export backward_stepwise
 
 """
 Mutable struct taking input as algorithm, params with an inner constructor.
-The inner constructor check if aic is given as input for param2 and creates a new ModelSelection object with param2 set as bic else aic.
+The inner constructor check if aic is given as input for param2 and creates a new ModelSelection
+object with param2 set as bic else aic.
 """
 mutable struct ModelSelection
     algorithm::Union{Function,Nothing}
@@ -25,22 +26,30 @@ mutable struct ModelSelection
     param2::Union{Function,Real,Nothing}
 
     ModelSelection(algorithm, deviance, aic, bic, param1, param2) =
-        typeof(aic) <: Nothing ? new(algorithm, deviance, aic, bic, deviance, bic) : new(algorithm, deviance, aic, bic, deviance, aic)
+        typeof(aic) <: Nothing ? new(algorithm, deviance, aic, bic, deviance, bic) :
+        new(algorithm, deviance, aic, bic, deviance, aic)
 end
 
+# An outer (default) constructor.
+# The outer (default) constructor sets algorithm to be Forward Stepwise, param1 to be deviance and param2 to be aic.
+# The inner constructor comes into play and set param2 as aic.
 """
-An outer (default) constructor.
-The outer (default) constructor sets algorithm to be Forward Stepwise, param1 to be deviance and param2 to be aic.
-The inner constructor comes into play and set param2 as aic.
+    ModelSelection()
+
+Creates ModelSelection(forward_stepwise, MLBase.deviance, MLBase.aic, nothing, nothing, nothing)
 """
 function ModelSelection()
     return ModelSelection(forward_stepwise, MLBase.deviance, MLBase.aic, nothing, nothing, nothing)
 end
 
+
+# An outer (main) constructor.
+# The outer (main) constructor inputs algorithm, param1 and param2 as strings
+# and then assigns corresponding functions using the dictionary and creates a ModelSelection object.
 """
-An outer (main) constructor.
-The outer (main) constructor inputs algorithm, param1 and param2 as strings
-and then assigns corresponding functions using the dictionary and creates a ModelSelection object.
+    ModelSelection(algorithm::AbstractString, param1::AbstractString, param2::AbstractString)
+
+Return a ModelSelection object
 """
 function ModelSelection(algorithm::AbstractString, param1::AbstractString, param2::AbstractString)
     dict = Dict([
@@ -59,16 +68,23 @@ function ModelSelection(algorithm::AbstractString, param1::AbstractString, param
         "deviance" => MLBase.deviance
     ])
     if param2 == "aic"
-        return ModelSelection(values(dict[lowercase(algorithm)]), values(dict[lowercase(param1)]), values(dict[lowercase(param2)]), nothing, nothing, nothing)
+        return ModelSelection(values(dict[lowercase(algorithm)]), values(dict[lowercase(param1)]),
+            values(dict[lowercase(param2)]), nothing, nothing, nothing)
     end
     if param2 == "bic"
-        return ModelSelection(values(dict[lowercase(algorithm)]), values(dict[lowercase(param1)]), nothing, values(dict[lowercase(param2)]), nothing, nothing)
+        return ModelSelection(values(dict[lowercase(algorithm)]), values(dict[lowercase(param1)]),
+            nothing, values(dict[lowercase(param2)]), nothing, nothing)
     end
 end
 
+
+# The fit function inputs the ModelSelection object created and data to be either A DataFrame or a matrix.
+# and updates values into 
 """
-The fit function inputs the ModelSelection object created and data to be either A DataFrame or a matrix.
-and updates values into 
+    fit(obj::ModelSelection, data::Union{DataFrame,AbstractMatrix{<:Real}})
+
+Fits the data to the inputed algorithm and parameter, returning a vector of vector containing
+indexes of the columns corresponding to least value of param 2.
 """
 function fit(obj::ModelSelection, data::Union{DataFrame,AbstractMatrix{<:Real}})
     dev = obj.algorithm(obj, data)
@@ -78,14 +94,23 @@ function fit(obj::ModelSelection, data::Union{DataFrame,AbstractMatrix{<:Real}})
         push!(final, obj.param2(logreg))
     end
     obj.param2 = minimum(final)
-    obj.aic = MLBase.aic(glm(Array(data[:, dev[indexin(minimum(final), final)[1]]]), Array(data[:, end]), Binomial(), ProbitLink()))
-    obj.bic = MLBase.bic(glm(Array(data[:, dev[indexin(minimum(final), final)[1]]]), Array(data[:, end]), Binomial(), ProbitLink()))
-    obj.deviance = MLBase.deviance(glm(Array(data[:, dev[indexin(minimum(final), final)[1]]]), Array(data[:, end]), Binomial(), ProbitLink()))
+    obj.aic = MLBase.aic(glm(Array(data[:, dev[indexin(minimum(final), final)[1]]]),
+        Array(data[:, end]), Binomial(), ProbitLink()))
+    obj.bic = MLBase.bic(glm(Array(data[:, dev[indexin(minimum(final), final)[1]]]),
+        Array(data[:, end]), Binomial(), ProbitLink()))
+    obj.deviance = MLBase.deviance(glm(Array(data[:, dev[indexin(minimum(final), final)[1]]]),
+        Array(data[:, end]), Binomial(), ProbitLink()))
     obj.param1 = obj.deviance
     return [dev[indexin(minimum(final), final)[1]]]
 end
 
 # Forward Step-wise Selection
+"""
+forward_stepwise(obj::ModelSelection, df::DataFrame)
+
+Executes the forward step-wise selection algorithm returning a vector of vectors containing
+indexes of columns corresponding to least value of param1.
+"""
 function forward_stepwise(obj::ModelSelection, df::DataFrame)
     dev = []
     comb = collect(combinations(1:length(names(df))-1, 1))
@@ -111,12 +136,24 @@ function forward_stepwise(obj::ModelSelection, df::DataFrame)
 end
 
 # Forward Step-wise Selection
+"""
+forward_stepwise(obj::ModelSelection, df::AbstractMatrix{<:Real})
+
+Executes the forward step-wise selection algorithm returning a vector of vectors containing
+indexes of columns corresponding to least value of param1.
+"""
 function forward_stepwise(obj::ModelSelection, df::AbstractMatrix{<:Real})
     df = DataFrame(df, :auto)
     forward_stepwise(obj, df)
 end
 
 # Best Subset Selection
+"""
+best_subset(obj::ModelSelection, df::DataFrame)
+
+Executes the best subset selection algorithm returning a vector of vectors containing
+indexes of columns corresponding to least value of param1.
+"""
 function best_subset(obj::ModelSelection, df::DataFrame)
     if size(df)[1] > size(df)[2]
         dev = []
@@ -135,6 +172,12 @@ function best_subset(obj::ModelSelection, df::DataFrame)
 end
 
 # Best Subset Selection
+"""
+best_subset(obj::ModelSelection, df::AbstractMatrix{<:Real})
+
+Executes the best subset selection algorithm returning a vector of vectors containing
+indexes of columns corresponding to least value of param1.
+"""
 function best_subset(obj::ModelSelection, df::AbstractMatrix{<:Real})
     df = DataFrame(df, :auto)
     if size(df)[1] > size(df)[2]
@@ -145,6 +188,12 @@ function best_subset(obj::ModelSelection, df::AbstractMatrix{<:Real})
 end
 
 # Backward Step-wise Selection
+"""
+backward_stepwise(obj::ModelSelection, df::DataFrame)
+
+Executes the backward step-wise selection algorithm returning a vector of vectors containing
+indexes of columns corresponding to least value of param1.
+"""
 function backward_stepwise(obj::ModelSelection, df::DataFrame)
     if size(df)[1] > size(df)[2]
         dev = [[s for s in 1:length(names(df))-1]]
@@ -165,6 +214,12 @@ function backward_stepwise(obj::ModelSelection, df::DataFrame)
 end
 
 # Backward Step-wise Selection
+"""
+backward_stepwise(obj::ModelSelection, df::DataFrame)
+
+Executes the backward step-wise selection algorithm returning a vector of vectors containing
+indexes of columns corresponding to least value of param1.
+"""
 function backward_stepwise(obj::ModelSelection, df::AbstractMatrix{<:Real})
     df = DataFrame(df, :auto)
     if size(df)[1] > size(df)[2]
