df18346387fb6194375aa01ea0dd595dafe10837
Himanshu <waitasecant@gmail.com>
2023-04-24 19:17:30 +0530

    Trying again!
    Last push resulted in warning msgs, which is weird since 1.8.0 src doesn't have any warnings. Works fine locally!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,9 @@
 using BestModelSubset
 using Test
-using DataFrames, Random
+using Random
+using DataFrames
 
-Random.seed!(31415)
+Random.seed!(314)
 df1 = hcat(rand(Float64, (50, 20)), rand([0, 1], (50, 1))) # n>p
 # df2 = hcat(rand(Float64, (10, 15)), rand([0, 1], (10, 1))) # n<p
 
@@ -11,9 +12,9 @@ df4 = hcat(rand(Float64, (10, 21))) # n<p
 
 @testset "Fit- Forward Step-wise" begin
     @test fit!(ModelSelection("forward", "deviance", "aic"), DataFrame(df1, :auto)) isa Array # DataFrame(n>p)
-    # @test fit!(ModelSelection("forward_stepwise", "deviance", "aic"), DataFrame(df2, :auto)) isa Array # DataFrame(n<p)
+    #     # @test fit!(ModelSelection("forward_stepwise", "deviance", "aic"), DataFrame(df2, :auto)) isa Array # DataFrame(n<p)
     @test fit!(ModelSelection("forward", "deviance", "bic"), df1) isa Array # Matrix(n>p)
-    # @test fit!(ModelSelection("forward_stepwise", "deviance", "bic"), df2) isa Array # Matrix(n<p)
+    #     # @test fit!(ModelSelection("forward_stepwise", "deviance", "bic"), df2) isa Array # Matrix(n<p)
 
     @test fit!(ModelSelection("forward", "r2", "aic"), DataFrame(df3, :auto)) isa Array # DataFrame(n>p)
     @test fit!(ModelSelection("forward_stepwise", "r2", "bic"), df3) isa Array # DataFrame(n>p)
@@ -25,9 +26,9 @@ end
 
 @testset "Fit- Best Subset" begin
     @test fit!(ModelSelection("bess", "deviance", "aic"), DataFrame(df1, :auto)) isa Array # DataFrame(n>p)
-    # @test fit!(ModelSelection("best", "deviance", "bic"), DataFrame(df2, :auto)) isa Array # DataFrame(n<p)
-    @test fit!(ModelSelection("best_subset", "deviance", "aic"), df1) isa Array # Matrix(n>p)
-    # @test fit!(ModelSelection("best_subset_selection", "deviance", "bic"), df2) isa Array # Matrix(n<p)
+    #     # @test fit!(ModelSelection("best", "deviance", "aic"), DataFrame(df2, :auto)) isa Array # DataFrame(n<p)
+    @test fit!(ModelSelection("best_subset", "deviance", "bic"), df1) isa Array # Matrix(n>p)
+    #     # @test fit!(ModelSelection("best_subset_selection", "deviance", "bic"), df2) isa Array # Matrix(n<p)
 
     @test fit!(ModelSelection("bess", "r2", "aic"), DataFrame(df3, :auto)) isa Array # DataFrame(n>p)
     @test fit!(ModelSelection("best", "r2", "bic"), df3) isa Array # DataFrame(n>p)
@@ -39,9 +40,9 @@ end
 
 @testset "Fit- Backward Step-wise" begin
     @test fit!(ModelSelection("backward", "deviance", "aic"), DataFrame(df1, :auto)) isa Array # DataFrame(n>p)
-    # @test fit!(ModelSelection("backward_stepwise", "deviance", "bic"), DataFrame(df2, :auto)) isa Array # DataFrame(n<p)
-    @test fit!(ModelSelection("backward", "deviance", "aic"), df1) isa Array # Matrix(n>p)
-    # @test fit!(ModelSelection("backward_stepwise", "deviance", "bic"), df2) isa Array # Matrix(n<p)
+    #     # @test fit!(ModelSelection("backward_stepwise", "deviance", "aic"), DataFrame(df2, :auto)) isa Array # DataFrame(n<p)
+    @test fit!(ModelSelection("backward", "deviance", "bic"), df1) isa Array # Matrix(n>p)
+    #     # @test fit!(ModelSelection("backward_stepwise", "deviance", "bic"), df2) isa Array # Matrix(n<p)
 
     @test fit!(ModelSelection("backward", "r2", "aic"), DataFrame(df3, :auto)) isa Array # DataFrame(n>p)
     @test fit!(ModelSelection("backward_stepwise", "r2", "bic"), df3) isa Array # DataFrame(n>p)
